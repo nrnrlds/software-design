c951d79f01b265b669efdaab54c28ae174497bca
nrnrlds <n.rulida.533869@umindanao.edu.ph>
2025-02-20 20:47:17 +0800

    commit

---
+++ Lab4Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab4Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Laboratory4_Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratory4_Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
